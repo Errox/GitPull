07b5e310b2152185094346750ac9a278515a9570
Ryan Groenewold <ryangroenewold@hotmail.com>
2016-10-01 23:34:25 +0200

    Github api added
    Composer update/install needed

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        GrahamCampbell\GitHub\GitHubServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -202,6 +204,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'GitHub' => GrahamCampbell\GitHub\Facades\GitHub::class,
 
     ],
 
